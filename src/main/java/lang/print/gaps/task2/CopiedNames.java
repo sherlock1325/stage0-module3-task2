04f09b1adf980bc177c2be58ba80d5e6fd65a587
sherlock1325 <106594627+sherlock1325@users.noreply.github.com>
2022-11-04 04:55:31 -0700

    commit2

--- src/main/java/lang/print/gaps/task2/CopiedNames.java
+++ src/main/java/lang/print/gaps/task2/CopiedNames.java
@@ -7,8 +7,7 @@ public class CopiedNames {
         int value = 1000;
         int variable3 = 4000;
         int value2 = 3000;
-        int variables = 5000;
-        variables = 7000;
+        int variables = 7000;
         int value3 = 90000;
     }
 }
--- src/main/java/lang/print/gaps/task2/advancedNamingConvention.java
+++ src/main/java/lang/print/gaps/task2/advancedNamingConvention.java
@@ -3,14 +3,10 @@ package lang.print.gaps.task2;
 public class advancedNamingConvention {
     private static final int adult_age = 18;
 
-    private int AGE;
-    private int phoneNumber;
-
-    void CAllToFriend() {
-        this.AGE = adult_age;
+    void CallToFriend() {
+        int age = adult_age;
     }
 
     void callByNumber(int Number) {
-        this.phoneNumber = Number;
     }
 }
